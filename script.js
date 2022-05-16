64834fd828f7026e0e142005f9c920b58d59a97e
Tshepho Mabusela <tshepho.mabusela@dadevs.co.za>
2022-05-16 12:47:47 +0200

    added images

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-const htmlCodeEditor = CodeMirror( document.querySelector('.code .html-code'), {
+const htmlCodeEditor = CodeMirror.fromTextArea( document.querySelector('.code .html-code'), {
     mode: "xml",
     theme: "dracula",
     lineNumbers: true,
     autoCloseTags: true
 } )
 
-const cssCodeEditor = CodeMirror( document.querySelector('.code .css-code'), {
+htmlCodeEditor.refresh()
+
+const cssCodeEditor = CodeMirror.fromTextArea( document.querySelector('.code .css-code'), {
     mode: "css",
     theme: "dracula",
     lineNumbers: true,
     autoCloseTags: true
 } )
 
-const jsCodeEditor = CodeMirror( document.querySelector('.code .js-code'), {
+cssCodeEditor.refresh()
+
+const jsCodeEditor = CodeMirror.fromTextArea( document.querySelector('.code .js-code'), {
     mode: "javascript",
     theme: "dracula",
     lineNumbers: true,
     autoCloseTags: true
 } )
+
+jsCodeEditor.refresh()
 
 
 document.querySelector('#run-btn').addEventListener('click', function(){
@@ -33,4 +39,29 @@ document.querySelector('#run-btn').addEventListener('click', function(){
 })
 
 
+let listItems = document.querySelectorAll('.nav-tabs li')
+let htmlItem = document.querySelector('.nav-tabs .html-item')
+let cssItem = document.querySelector('.nav-tabs .css-item')
+let jsItem = document.querySelector('.nav-tabs .js-item')
 
+listItems.forEach(item => {
+    console.log(item)
+    item.addEventListener('click', (e) => {
+       if( e.target.classList.contains('html-item') || e.target.classList.contains('html-item-img') ){
+                htmlItem.style.backgroundColor = '#44475a';
+                cssItem.style.backgroundColor = 'transparent';
+                jsItem.style.backgroundColor = 'transparent';
+                console.log('html block selected')
+       } else if( e.target.classList.contains('css-item') || e.target.classList.contains('css-item-img') ){
+                 htmlItem.style.backgroundColor = 'transparent';
+                 cssItem.style.backgroundColor = '#44475a';
+                 jsItem.style.backgroundColor = 'transparent';
+            console.log('css block selected')
+       } else if( e.target.classList.contains('js-item') || e.target.classList.contains('js-item-img') ){
+                htmlItem.style.backgroundColor = 'transparent';
+                cssItem.style.backgroundColor = 'transparent';
+                jsItem.style.backgroundColor = '#44475a';
+             console.log('js block selected')
+       }
+    })
+})
